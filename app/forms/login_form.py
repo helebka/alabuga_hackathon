f37af12874e42c3426618560da27e386e2b1711b
Helebka <kasilovgleb7@gmail.com>
2024-07-01 11:30:30 +0300

    Delete trash

--- app/forms/login_form.py
+++
@@ -1,9 +0,0 @@
-from wtforms import EmailField, SubmitField, PasswordField
-from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    email = EmailField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Log In")
